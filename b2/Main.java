b8cec99fa8519fc990888c95be82dac8199eda0e
haiyen2304 <hhyhoanhaiyen2304@gmail.com>
2024-08-28 23:22:37 +0700

    e nop bt

---
+++ b2/Main.java
@@ -0,0 +1,21 @@
+package homeworks.b2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        //Tim phan tu duy nhat
+        int[] mang ={8,8,8,0,34,0,6,5,4,8,5};
+        Map<Integer,Integer> list = new HashMap<>();
+
+        for (int x : mang){
+            if(list.containsKey(x)){
+                list.remove(x);
+            }else {
+                list.put(x,1);
+            }
+        }
+        System.out.println(list);
+    }
+}
---
+++ b3/Main.java
@@ -0,0 +1,13 @@
+package homeworks.b3;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer, String> hashMap = new HashMap<>();
+//        List<Map.Entry<String, Integer>> entryList = new ArrayList<>(hashMap.entrySet());
+    }
+}
